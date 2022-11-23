0208e47f16302d046d57e6911eba73c4545ca672
Paweł Wygoda <wygoda.pawel@gmail.com>
2022-11-23 18:04:36 +0100

    stash changes

--- chart/src/acquisitions.js
+++ chart/src/acquisitions.js
@@ -4,15 +4,12 @@ import newData from '../../job_scrapper/output.json'
 
 (async function() {
 
-  //console.log('my data - ', newData)
-
-
   let data = []
 
   newData.forEach(job => {
     if (data.find(x => x?.company === job?.company)) {
       let position = data.findIndex(x => x?.company === job?.company) 
-      data[position].numberOfOffers += 1
+      data[position].numberOfOffers++
     } else {
       data.push({company: job.company, numberOfOffers: 1})
     }
@@ -20,17 +17,6 @@ import newData from '../../job_scrapper/output.json'
 
   data.sort((a, b) => b.numberOfOffers - a.numberOfOffers)
   const slicedData = data.slice(0, 20)
-
-
-  // const data = [
-  //   { year: 2010, count: 10 },
-  //   { year: 2011, count: 20 },
-  //   { year: 2012, count: 15 },
-  //   { year: 2013, count: 25 },
-  //   { year: 2014, count: 22 },
-  //   { year: 2015, count: 30 },
-  //   { year: 2016, count: 28 },
-  // ];
 
   new Chart(
     document.getElementById('acquisitions'),
