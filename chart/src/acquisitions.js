cb1928fd29ff71f29f7d4639bb34476d5d7880d6
Paweł Wygoda <wygoda.pawel@gmail.com>
2022-11-26 17:11:05 +0100

    second and third chart added

--- chart/src/acquisitions.js
+++ chart/src/acquisitions.js
@@ -1,13 +1,13 @@
 
 import { Chart } from 'chart.js/auto'
-import { getJobsByCompany } from './query.js'
+import { getJobsByCompany, getJobsFromByCity, languageNumber } from './query.js'
 
 (async function() {
 
   // First Query for Number of Jobs posted by each company
-  const numberOfJobs = getJobsByCompany()
+  const numberOfJobsFromCompany = getJobsByCompany()
   new Chart(
-    document.getElementById('acquisitions'),
+    document.getElementById('acquisitions1'),
     {
       type: 'bar',
       options: {
@@ -22,17 +22,18 @@ import { getJobsByCompany } from './query.js'
         }
       },
       data: {
-        labels: numberOfJobs.map(row => row.company),
+        labels: numberOfJobsFromCompany.map(row => row.company),
         datasets: [
           {
-            label: 'Number of Jobs',
-            data: numberOfJobs.map(row => row.numberOfOffers)
+            label: 'Number of Jobs by Companies',
+            data: numberOfJobsFromCompany.map(row => row.numberOfOffers)
           }
         ]
       }
     }
   );
 
+  const numberOfJobsFromCity = getJobsFromByCity()
   // Second Query
   new Chart(
     document.getElementById('acquisitions2'),
@@ -50,11 +51,43 @@ import { getJobsByCompany } from './query.js'
         }
       },
       data: {
-        labels: numberOfJobs.map(row => row.company),
+        labels: numberOfJobsFromCity.map(row => row.location),
         datasets: [
           {
-            label: 'Number of Jobs',
-            data: numberOfJobs.map(row => row.numberOfOffers)
+            label: 'Number of Job by Cities',
+            data: numberOfJobsFromCity.map(row => row.numberOfOffers),
+            borderColor: '#FE3212',
+            borderWidth: 3
+          }
+        ]
+      }
+    }
+  );
+
+  const numberOfLanguages = languageNumber()
+  // Third Query
+  new Chart(
+    document.getElementById('acquisitions3'),
+    {
+      type: 'bar',
+      options: {
+        animation: true,
+        plugins: {
+          legend: {
+            display: true
+          },
+          tooltip: {
+            enabled: true
+          }
+        }
+      },
+      data: {
+        labels: numberOfLanguages.map(row => row.language),
+        datasets: [
+          {
+            label: 'Most seeked technology',
+            data: numberOfLanguages.map(row => row.number),
+            hoverOffset: 4
           }
         ]
       }
--- chart/src/query.js
+++ chart/src/query.js
@@ -15,7 +15,7 @@ export const getJobsByCompany = () => {
     })
   
     data.sort((a, b) => b.numberOfOffers - a.numberOfOffers)
-    const slicedData = data.slice(0, 10)
+    const slicedData = data.slice(0, 15)
   
     return slicedData
   } catch (error) {
@@ -24,4 +24,52 @@ export const getJobsByCompany = () => {
 }
 
 // Second Chart
-export const getJobsFromByCity = () => {}
+export const getJobsFromByCity = () => {
+  try {
+    let data = []
+  
+    newData.forEach(job => {
+      if (data.find(x => x?.location === job?.location)) {
+        let position = data.findIndex(x => x?.location === job?.location) 
+        data[position].numberOfOffers++
+      } else {
+        data.push({location: job.location, numberOfOffers: 1})
+      }
+    })
+  
+    data.sort((a, b) => b.numberOfOffers - a.numberOfOffers)
+    const slicedData = data.slice(0, 15)
+    // console.log('slicedData - ', slicedData)
+  
+    return slicedData
+  } catch (error) {
+    console.error('Error - ', error)
+  }
+}
+
+// Third Chart
+export const languageNumber = () => {
+  try {
+    let data = []
+  
+    newData.forEach(job => {
+      job.tags.forEach(language => {
+        if (data.find(x => x.language === language)) {
+          let position = data.findIndex(x => x?.language === language) 
+          data[position].number += 1
+        } else {
+          data.push({language: language, number: 1})
+        }
+
+      })
+    })
+  
+    data.sort((a, b) => b.number - a.number)
+    const slicedData = data.slice(0, 15)
+    // console.log('slicedData - ', slicedData)
+  
+    return slicedData
+  } catch (error) {
+    console.error('Error - ', error)
+  }
+}
